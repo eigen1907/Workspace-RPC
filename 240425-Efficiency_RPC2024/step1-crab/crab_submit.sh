fc617a0512579d8203fe9c29894ade071c142b98
Jongwon <sourcemaru@gmail.com>
2024-04-26 18:16:13 +0200

    update 0426

---
+++ 240425-Efficiency_RPC2024/step1-crab/crab_submit.sh
@@ -0,0 +1,6 @@
+rpc-crab-submit.py \
+    -p ${CMSSW_BASE}/src/RPCDPGAnalysis/NanoAODTnP/test/muRPCTnPFlatTableProducer_cfg.py \
+    -i ${CMSSW_BASE}/src/RPCDPGAnalysis/NanoAODTnP/data/crab/run3.json \
+    -s T3_CH_CERNBOX \
+    -u joshin \
+    -n TnP-NanoAOD
