95d5960661bc8abf6b65cd1e3d716cb4577d92a6
Jongwon Shin <sourcemaru@gmail.com>
2024-05-01 14:35:34 +0900

    update 240501

--- 240425-TnP_RPC24/240429-crab_knu/step1-crab/crab_resubmit.sh
+++
@@ -1,5 +0,0 @@
-for f in crab_muRPCTnPFlatTableProducer_cfg*; do
-  echo "--------------------------------------------"
-  echo "Resubmit -> $f"
-  crab resubmit $f --numcores 8 --maxjobruntime=3000
-done
