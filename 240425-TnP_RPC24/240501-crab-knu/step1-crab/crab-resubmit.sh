11696ed554eceb7efe8be49b40c11a6bebacce49
Jongwon Shin <sourcemaru@gmail.com>
2024-05-03 16:36:04 +0900

    update 0503

--- 240425-TnP_RPC24/240501-crab-knu/step1-crab/crab-resubmit.sh
+++ 240425-TnP_RPC24/240501-crab-knu/step1-crab/crab-resubmit.sh
@@ -1,5 +1,5 @@
 for f in crab_muRPCTnPFlatTableProducer_cfg*; do
   echo "--------------------------------------------"
   echo "Resubmit -> $f"
-  crab resubmit $f --siteblacklist=['T2_DE_DESY']
+  crab resubmit $f --siteblacklist=T2_DE_DESY
 done
