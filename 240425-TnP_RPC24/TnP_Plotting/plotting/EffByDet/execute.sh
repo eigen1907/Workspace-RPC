be79287f1fe438966f667831325e648726966e72
Jongwon Shin <sourcemaru@gmail.com>
2024-05-18 18:28:15 +0900

    update 0518

--- 240425-TnP_RPC24/TnP_Plotting/plotting/EffByDet/execute.sh
+++ 240425-TnP_RPC24/TnP_Plotting/plotting/EffByDet/execute.sh
@@ -1,8 +1,8 @@
 cmsenv
-mkdir Run2023
+mkdir Run2022
 rpc-tnp-plot-eff-detector.py \
-    -i ${CMSSW_BASE}/Workspace-RPC/240425-TnP_RPC24/TnP_Plotting/data/Run2023.root \
+    -i ${CMSSW_BASE}/Workspace-RPC/240425-TnP_RPC24/TnP_Plotting/data/Run2022.root \
     -g ${CMSSW_BASE}/Workspace-RPC/240425-TnP_RPC24/TnP_Plotting/geometry/run3.csv \
     -s 13.6 \
     -y 2022 \
-    -o Run2023
+    -o Run2022
