72455303e94e9248f8f8b6bf58454d4d651bed1d
Jongwon Shin <sourcemaru@gmail.com>
2024-05-19 00:21:47 +0900

    update 0519

--- 240425-TnP_RPC24/TnP_Plotting/plotting/EffByDet/execute.sh
+++ 240425-TnP_RPC24/TnP_Plotting/plotting/EffByDet/execute.sh
@@ -1,8 +1,8 @@
 cmsenv
-mkdir Run2022
 rpc-tnp-plot-eff-detector.py \
     -i ${CMSSW_BASE}/Workspace-RPC/240425-TnP_RPC24/TnP_Plotting/data/Run2022.root \
     -g ${CMSSW_BASE}/Workspace-RPC/240425-TnP_RPC24/TnP_Plotting/geometry/run3.csv \
     -s 13.6 \
     -y 2022 \
-    -o Run2022
+    -o Run2022 \
+    --roll-blacklist-path ${CMSSW_BASE}/Workspace-RPC/240425-TnP_RPC24/TnP_Plotting/blacklist/roll-blacklist.json
