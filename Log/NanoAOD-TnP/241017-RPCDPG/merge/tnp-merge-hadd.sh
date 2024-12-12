da225599f386903761565daff2c814a6b43b066f
Jongwon Shin <eigen1907@gmail.com>
2024-12-12 10:08:19 +0100

    update 241212

--- Log/NanoAOD-TnP/241017-RPCDPG/merge/tnp-merge-hadd.sh
+++ Log/NanoAOD-TnP/241017-RPCDPG/merge/tnp-merge-hadd.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
-TYPES=(without_blacklist with_blacklist_roll with_blacklist_roll_run with_blacklist_roll_run_RE4)
-#TYPES=(with_blacklist_roll with_blacklist_roll_run)
-#TYPES=(with_blacklist_roll_run_RE4)
-for TYPE in ${TYPES[@]}
-do
-    SOURCE_DIR=/users/hep/eigen1907/store/TnP-NanoAOD/flatten_output/${TYPE}
-    TARGET_DIR=/users/hep/eigen1907/store/TnP-NanoAOD/analysis/${TYPE}
 
-    mkdir -p ${TARGET_DIR}
+INPUT_DIR=/eos/user/j/joshin/RPC-TnP-Flat-NanoAOD
+OUTPUT_DIR=/eos/user/j/joshin/RPC-TnP-Flat-NanoAOD-merged
 
-    for DATA_TYPE in $(ls ${SOURCE_DIR})
-    do
-        for ERA_DATA in $(ls ${SOURCE_DIR}/${DATA_TYPE})
-        do
-            hadd ${TARGET_DIR}/${DATA_TYPE}_${ERA_DATA}.root ${SOURCE_DIR}/${DATA_TYPE}/${ERA_DATA}/*/*
+mkdir ${OUTPUT_DIR}
+for DATA_TYPE in $(ls ${INPUT_DIR}); do
+    for ERA in $(ls ${INPUT_DIR}/${DATA_TYPE}); do
+        mkdir ${OUTPUT_DIR}/${DATA_TYPE}-${ERA}
+        for PREFIX in $(ls ${INPUT_DIR}/${DATA_TYPE}/${ERA}/*); do
+            hadd -f ${OUTPUT_DIR}/${DATA_TYPE}-${ERA}/${PREFIX}.root ${INPUT_DIR}/${DATA_TYPE}/${ERA}/*/${PREFIX}/*.root
         done
     done
-
-    hadd ${TARGET_DIR}/Run2022.root ${TARGET_DIR}/*Run2022*
-    hadd ${TARGET_DIR}/Run2023.root ${TARGET_DIR}/*Run2023*
-    hadd ${TARGET_DIR}/Run3.root ${TARGET_DIR}/Run2022.root ${TARGET_DIR}/Run2023.root
 done
+
+#YEARS=(2022 2023 2024)
+#for YEAR in ${YEARS[@]}; do
+#    hadd Run${YEAR}.root */*${YEAR}*.root
+#done
